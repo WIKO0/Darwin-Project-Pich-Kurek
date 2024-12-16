3891cce0a37d442f1d7ae37b61eaccd6f670968f
PiotrPich2024 <ppich@student.agh.edu.pl>
2024-12-16 17:52:25 +0100

    dodanie MoveD

---
+++ Project-Darwin/src/main/java/EnumClasses/MoveDirections.java
@@ -0,0 +1,12 @@
+package EnumClasses;
+
+public enum MoveDirections {
+    FORWARD,
+    FORWARD_RIGHT,
+    FORWARD_LEFT,
+    BACKWARD,
+    RIGHT,
+    LEFT,
+    BACKWARD_RIGHT,
+    BACKWARD_LEFT,
+}
