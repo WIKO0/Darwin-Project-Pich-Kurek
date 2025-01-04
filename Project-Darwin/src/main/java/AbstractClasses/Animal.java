571f2e4831a11228008902a766c5db8f257b9051
PiotrPich2024 <ppich@student.agh.edu.pl>
2025-01-04 19:16:27 +0100

    wiek zwierze

--- Project-Darwin/src/main/java/AbstractClasses/Animal.java
+++ Project-Darwin/src/main/java/AbstractClasses/Animal.java
@@ -8,23 +8,45 @@ import java.util.Random;
 
 public class Animal extends AbstractAnimal{
     private int energy;
-    private int speed;
+    private int numberOfMoves;
     private int age;
-    public Animal(Vector2D pos, int n, int energy){
+    private int paceOfAging;
+
+    public Animal(Vector2D pos, int n, int energy, int paceOfAging) {
         this.position = pos;
         Random rand = new Random();
         this.direction = direction.fromRadians(rand.nextInt(8) * 45);
         this.genes = new Genes(n);
         this.energy = energy;
+        this.numberOfMoves = 0;
+        this.paceOfAging = paceOfAging;
     }
 
 
 
     @Override
     public boolean canMove() {
-        move();
-        return true;
+        numberOfMoves++;
+        energy -= 1;
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100);
+        if(randomNum < Math.min(age,79)){
+            move();
+            return true;
+        };
+        return false;
     }
+
+    public void aging(){
+        if (numberOfMoves % paceOfAging == 0) {
+            age ++;
+        }
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+    
 
     @Override
     public void addObserver(PositionChangeObserver observer) {
